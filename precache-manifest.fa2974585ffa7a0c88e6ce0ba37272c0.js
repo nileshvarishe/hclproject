aa6d5f381842a5daf98f9b0e63a9813273df0a50
nileshvarishe <nileshvarishe@gmail.com>
2020-07-21 10:56:22 +0530

    Updates

---
+++ precache-manifest.fa2974585ffa7a0c88e6ce0ba37272c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479975a110898d816f3521b3bb115520",
+    "url": "/hclproject/index.html"
+  },
+  {
+    "revision": "edf83bed1f6558f34841",
+    "url": "/hclproject/static/css/main.ca3a0e01.chunk.css"
+  },
+  {
+    "revision": "824356931744c21d07ff",
+    "url": "/hclproject/static/js/2.1368f937.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hclproject/static/js/2.1368f937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf83bed1f6558f34841",
+    "url": "/hclproject/static/js/main.62d6e0bf.chunk.js"
+  },
+  {
+    "revision": "3d149ce95f26c49ec52f",
+    "url": "/hclproject/static/js/runtime-main.eb7def33.js"
+  }
+]);
